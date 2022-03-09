30f183b6809965b16caacf4e6e3bbbc322f39d59
ainvnt <100088249+ainvnt@users.noreply.github.com>
2022-03-09 20:16:06 +0530

    on scroll sticky eader added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { MenuComponent } from './header/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
